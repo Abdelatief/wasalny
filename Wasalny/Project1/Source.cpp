c1fd9e680c67bb1646845e50b2cbdb1079ad7da7
Abdelrahman Ahmed <abdelrahman@me>
2018-12-07 19:25:55 +0200

    some new comments

--- Wasalny/Project1/Source.cpp
+++ Wasalny/Project1/Source.cpp
@@ -31,7 +31,8 @@ int main()
 	adjlist.resize(n + 1);
 	source.resize(n + 1);
 	cost.assign(n + 1, 1e8);
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < m; i++)
+	{
 		int x, y;
 		long long w;
 		cin >> x >> y >> w;
@@ -39,15 +40,18 @@ int main()
 	}
 	source[1] = 1;
 	cost[1] = 0;
-	pq.push({ 0,1 });
-	while (!pq.empty()) {
+	pq.push({ 0,1 }); // pair {cost, source}
+	while (!pq.empty())
+	{
 		int node = pq.top().second;
 		pq.pop();
-		for (int i = 0; i < adjlist[node].size(); i++) {
+		for (int i = 0; i < adjlist[node].size(); i++)
+		{
 			int child = adjlist[node][i].first;
 			int childW = adjlist[node][i].second;
-			if (childW + cost[node] < cost[child]) {
-				cost[child] = childW + cost[node];
+			if (childW + cost[node] < cost[child]) // (child weight + cost of the node ) > cost of the child
+			{
+				cost[child] = childW + cost[node]; // child weight = weight of the edge
 				source[child] = node;
 				pq.push({ -1 * cost[child], child });
 			}
@@ -56,10 +60,12 @@ int main()
 	stack <int> st;
 	st.push(n);
 	int x = source[n];
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++)
+	{
 		st.push(x);
 		x = source[x];
-		if (x == source[x]) {
+		if (x == source[x]) 
+		{
 			st.push(x);
 			break;
 		}
@@ -68,5 +74,6 @@ int main()
 		cout << st.top() << " ";
 		st.pop();
 	}
+	system("pause");
 	return 0;
 }
